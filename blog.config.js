fedcef2536794586be0ae922f80fd80ef59bdaee
michaelyuhe <2312744987@qq.com>
2023-02-14 09:49:46 +0800

    wip

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: 'MichaelYuhe', // 作者
-  BIO: 'Frontend Developer, Student, Zeabur Co-Founder', // 作者简介
+  BIO: 'A Frontend Developer, A Student', // 作者简介
   // LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
