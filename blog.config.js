2425713e0319a325d1f7bd243d9e4c6db4661dae
michaelyuhe <2312744987@qq.com>
2023-02-24 14:46:53 +0800

    wip

--- blog.config.js
+++ blog.config.js
@@ -4,21 +4,20 @@ const BLOG = {
   NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || '5ea00bc39efd4b4fbf88133cb2c63e0e',
   PSEUDO_STATIC: false, // 伪静态路径，开启后所有文章URL都以 .html 结尾。
   NEXT_REVALIDATE_SECOND: process.env.NEXT_PUBLIC_REVALIDATE_SECOND || 5, // 更新内容缓存间隔 单位(秒)；即每个页面有5秒的纯静态期、此期间无论多少次访问都不会抓取notion数据；调大该值有助于节省Vercel资源、同时提升访问速率，但也会使文章更新有延迟。
-  THEME: process.env.NEXT_PUBLIC_THEME || 'hexo', // 主题， 支持 ['next','hexo',"fukasawa','medium','example'] @see https://preview.tangly1024.com
+  THEME: process.env.NEXT_PUBLIC_THEME || 'medium', // 主题， 支持 ['next','hexo',"fukasawa','medium','example'] @see https://preview.tangly1024.com
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
   LANG: 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: 2023, // e.g if leave this empty, current year will be used.
   APPEARANCE: 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
-  AUTHOR: 'Zeabur', // 作者
-  BIO: 'Zeabur Team', // 作者简介
-  // LINK: 'https://tangly1024.com', // 网站地址
-  KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
-  // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
+  AUTHOR: 'MichaelYuhe', // 作者
+  BIO: 'Frontend Dev, Freelancer, Student', // 作者简介
+  LINK: 'https://michaelyuhe.zeabur.app', // 网站地址
+  KEYWORDS: 'Notion, 博客, Zeabur, 前端', // 网站关键词 英文逗号隔开
   CONTACT_EMAIL: 'xiayuhang1106@gmail.com',
-  CONTACT_TWITTER: 'https://twitter.com/zeaburapp',
-  CONTACT_GITHUB: 'https://github.com/<zeabur>',
-  CONTACT_TELEGRAM: 'https://t.me/zeabur_app',
+  CONTACT_TWITTER: 'https://twitter.com/cooolxyh',
+  CONTACT_GITHUB: 'https://github.com/<MichaelYuhe>',
+  // CONTACT_TELEGRAM: 'https://t.me/zeabur_app',
 
   // 网站字体
   FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
